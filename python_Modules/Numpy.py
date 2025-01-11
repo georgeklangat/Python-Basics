7b25a04ecd22999d188db179795c02cc00c750d1
georgeklangat <georgeklangat@gmail.com>
2025-01-11 07:04:47 +0300

    edit the library

--- python_Modules/Numpy.py
+++ python_Modules/Numpy.py
@@ -1,7 +1,7 @@
 # numpy-numerical python-this is the library for working with array
 # the array object in Numpy is called ndarray
 
-import Numpy as np
+import numpy as np
 
 arr= np.array([1,2,3,4,5])
 print(arr)
